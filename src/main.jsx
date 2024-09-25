6c451c70b069b596e1d94d70081c3180d4fc440a
dang tien <halelugia96@gmail.com>
2024-09-25 13:52:17 +0700

    hello

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import AppProvider from "./context/index.jsx";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import AppProvider from './context/index.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+ direction: 'rtl',
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ <React.StrictMode>
+  <BrowserRouter>
+   <AppProvider>
+    <ThemeProvider theme={theme}>
+     <App />
+    </ThemeProvider>
+   </AppProvider>
+  </BrowserRouter>
+ </React.StrictMode>
+)
